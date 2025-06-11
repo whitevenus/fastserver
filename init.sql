926a8b1a5b51bd9dafe94b681c8cefe04fdd7c45
母文涛 <muwentao@Pro.local>
2025-06-11 16:35:56 +0800

    fix some bug

--- init.sql
+++ init.sql
@@ -10,11 +10,13 @@ CREATE TABLE IF NOT EXISTS `student` (
     `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_id` INT NOT NULL DEFAULT 0 COMMENT '更新人ID',
     `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `note` VARCHAR(255) DEFAULT NULL COMMENT '备注'
+    `note` VARCHAR(255) DEFAULT NULL DEFAULT "" COMMENT '备注'
 ) COMMENT '学生表';
 
 -- 插入数据
 INSERT INTO `student` (`name`, `age`, note) VALUES
-('张三', 20, '这是张三的备注'),
-('李四', 22, '这是李四的备注'),
-('王五', 21, '这是王五的备注');
+('alex', 20, 'this is a test note'),
+('bob', 21, 'this is a test note'),
+('cathy', 22, 'this is a test note'),
+('david', 23, 'this is a test note'),
+('eve', 24, 'this is a test note');
